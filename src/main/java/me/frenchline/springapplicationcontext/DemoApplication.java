f81b53eb6095cc2d5d95f309c604863195b3bd46
Lucy <frenchline707@gmail.com>
2020-12-02 18:41:38 +0900

    IoC 컨테이너 2: ApplicationContext와 다양한 빈 설정 방법 (스프링 부트)
    @SpringBootApplication

--- src/main/java/me/frenchline/springapplicationcontext/DemoApplication.java
+++ src/main/java/me/frenchline/springapplicationcontext/DemoApplication.java
@@ -1,15 +1,22 @@
 package me.frenchline.springapplicationcontext;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.Arrays;
 
+@SpringBootApplication
 public class DemoApplication {
 
+    //@SpringBootApplication이 붙어있다면 ApplicationContext도 스프링이 알아서 만들어 준다
+    //@SpringBootApplication 안에 @ComponentScan, @Configuration이 다 포함되어 있다
+    //@SpringBootApplication이 붙어있는 현재 클래스 자체가 사실상 Bean 설정 파일이다.
+    //이전에 만든 ApplicationConfig이 사실 필요없게 된다
+
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class); //Bean 설정 클래스를 생성자 파라미터로 넘겨준다
+        ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 
         String[] beanDefinitionNames = context.getBeanDefinitionNames();
         System.out.println(Arrays.toString(beanDefinitionNames));
